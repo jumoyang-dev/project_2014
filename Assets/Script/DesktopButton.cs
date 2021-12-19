64b1ad5ed591f0a3ba1a5c9db4be5dc583056264
lyuflora <arislingo@163.com>
2021-12-19 00:53:54 -0500

    Revert "update"
    This reverts commit 7a8a2ca4f9cf4a48e3b6cd7212eecc7255351583.

--- Assets/Script/DesktopButton.cs
+++ Assets/Script/DesktopButton.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DesktopButton : MonoBehaviour
 {
-
+<<<<<<< Updated upstream
     public float highlightTime = 7.0f;
     private Animator btAnimator;
     public string highlightTrigger = "highlight";
@@ -14,31 +14,50 @@ public class DesktopButton : MonoBehaviour
     private void Start()
     {
         btAnimator = GetComponent<Animator>();
+=======
+    public Animator btAnimator;
+    public float highlightTime = 7.0f;
+    private Sprite sprite;
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>().sprite;
+>>>>>>> Stashed changes
     }
 
     public void Highlight()
     {
+<<<<<<< Updated upstream
         isDisabled = false;
         btAnimator.SetTrigger(highlightTrigger);
         StartCoroutine(PlayAnimByTime(highlightTime, highlightTrigger));
+
     }
 
     public IEnumerator PlayAnimByTime(float time, string trigger)
     {
         yield return new WaitForSeconds(time);
         btAnimator.SetTrigger(trigger);
+=======
+        GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+        btAnimator.SetTrigger("highlight");
+        StartCoroutine(HighlightTime(highlightTime));
+
     }
 
     public IEnumerator HighlightTime(float time)
     {
         yield return new WaitForSeconds(time);
         btAnimator.SetTrigger("highlight");
+>>>>>>> Stashed changes
     }
 
     public void Disabled()
     {
+<<<<<<< Updated upstream
         isDisabled = true;
         btAnimator.SetTrigger(disabledTrigger);
+=======
+        GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 183);
     }
 
     public void Normal()
@@ -50,5 +69,6 @@ public class DesktopButton : MonoBehaviour
     public void Hover()
     {
 
+>>>>>>> Stashed changes
     }
 }
